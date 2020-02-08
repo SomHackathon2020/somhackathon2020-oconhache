99fde585ea88138f01839b80535f6a37b56483b4
David <dvolart@edu.tecnocampus.cat>
2020-02-08 13:34:57 +0100

    Set-Get User

--- app/src/main/java/com/example/oconh/User.java
+++ app/src/main/java/com/example/oconh/User.java
@@ -9,4 +9,20 @@ public class User {
         this.name = name;
         this.password = password;
     }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
 }
