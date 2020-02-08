c20af96f94082f66f5e7279ca59dcb42267a0a28
lia25 <lalavrezc@edu.tecnocampus.cat>
2020-02-08 18:41:55 +0100

    eventDetail

--- app/src/main/java/com/example/oconh/SingUpActivity.java
+++ app/src/main/java/com/example/oconh/SingUpActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SingUpActivity extends Activity {
 
@@ -17,6 +18,7 @@ public class SingUpActivity extends Activity {
     private EditText user_mail;
     private Button register;
     private ImageView imageView;
+
 
     private UserService userService = new UserService();
 
