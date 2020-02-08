cc931dc89bb2d21778bb91996962e6022ad75944
lia25 <lalavrezc@edu.tecnocampus.cat>
2020-02-08 17:46:40 +0100

    Merge branch 'develop' of https://github.com/SomHackathon2020/somhackathon2020-oconhache into develop
    # Conflicts:
    #	app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml
    #	app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml

--- app/src/main/java/com/example/oconh/Logo.java
+++ app/src/main/java/com/example/oconh/Logo.java
@@ -12,11 +12,7 @@ public class Logo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
         String entrado="";
         Intent ii=new Intent(getApplicationContext(), MainActivity.class);
         ii.putExtra("name", entrado);
