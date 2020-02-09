5a2207612a37213574d8d3f9df7c82a3301e3daa
edu1010 <edu1010.ecl@gmail.com>
2020-02-09 01:33:13 +0100

    list

--- app/src/main/java/com/example/oconh/QueFuncioneEstoVa.java
+++ app/src/main/java/com/example/oconh/QueFuncioneEstoVa.java
@@ -9,12 +9,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class QueFuncioneEstoVa extends AppCompatActivity {
 
 
     private ListView lv1;
     private  String eventos []={};
     private  String info []={};
+    private Adaptador adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +26,10 @@ public class QueFuncioneEstoVa extends AppCompatActivity {
 
         lv1 = (ListView)findViewById(R.id.lv1);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.list_item_evento,eventos);
-        lv1.setAdapter(adapter);
+        adaptador =new Adaptador(this, GeTarrayItems());
+
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.list_item_evento,eventos);
+        lv1.setAdapter(adaptador);
 
         lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -32,6 +37,11 @@ public class QueFuncioneEstoVa extends AppCompatActivity {
                 //tv1.setText("La edad de " + lv1.getItemAtPosition(i) + " es " + edades[i] + " años" );
             }
         });
+    }
+    private ArrayList<Entidad> GeTarrayItems(){
+        ArrayList<Entidad> listItems=new ArrayList<>();
+        //listItems.add(new Entidad(id foto,titulo, descripcion))
+        return listItems;
     }
 
 }
